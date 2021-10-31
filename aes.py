0de6a48c0900c1b6fafba443b30c7630cbf5c3fe
Pham Phuoc <phhphc@gmail.com>
2021-10-31 20:28:09 +0700

    key expand return list of byte instead of word

--- aes.py
+++ aes.py
@@ -80,8 +80,14 @@ def keyExpand(key):
                 words[i] = words[i-n]^subWord(words[i-1])
             else: 
                 words[i] = words[i-n]^words[i-1]
-        return [[words[i + j*4] for i in range(4)] for j in range(round)]
+        
+        byte = []
+        for w in words: 
+            byte += wordToByte(w)
+        return [ [byte[j*16 + i] for i in range(16)] for j in range(round)]
 
     return expand(key)
+
+
 
 print(keyExpand(hexToWords('00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f 10 11 12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f'.replace(' ', ''))))
